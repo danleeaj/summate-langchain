36e051b0dee2293b08e02503ec38a05ef2089300
Daniel Lee <anjie.wav@gmail.com>
2025-02-15 15:12:01 -0800

    Changed rubric component and student response to be global constants

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import yaml
 from app.utils.get_evaluation import get_evaluation
 from app.models.query_model import Query
 
+# Load config.yaml, read values and store them into constants
+
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+
+RUBRIC_COMPONENT = config['rubricComponent'][1]['component']
+STUDENT_RESPONSE = config['studentResponse'][0]
+
 def main():
-
-    # Load config.yaml, read values and store them into constants
-
-    with open("config.yaml", "r") as f:
-        config = yaml.safe_load(f)
-
-    RUBRIC_COMPONENT = config['rubricComponent'][0]['component']
-    STUDENT_RESPONSE = config['studentResponse'][0]
 
     response, path = get_evaluation(Query(
         rubricComponent=RUBRIC_COMPONENT,
