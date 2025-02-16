89f3b6051d394f593c48bc6963cd8cc2b25af752
Daniel Lee <anjie.wav@gmail.com>
2025-02-16 14:36:14 -0800

    Test database manager functionality by adding a sample question

--- test.py
+++ test.py
@@ -1,32 +1,39 @@
-import yaml
-from app.utils.get_evaluation import get_evaluation, evaluate_question
-from app.models.query_model import Query
+# import yaml
+# from app.utils.get_evaluation import get_evaluation, evaluate_question
+# from app.models.query_model import Query
 
-# Load config.yaml, read values and store them into constants
+# # Load config.yaml, read values and store them into constants
 
-with open("config.yaml", "r") as f:
-    config = yaml.safe_load(f)
+# with open("config.yaml", "r") as f:
+#     config = yaml.safe_load(f)
 
-RUBRIC_COMPONENT = config['rubricComponent'][1]['component']
-RUBRIC_COMPONENTS = config['rubricComponent']
-STUDENT_RESPONSE = config['studentResponse'][0]
+# RUBRIC_COMPONENT = config['rubricComponent'][1]['component']
+# RUBRIC_COMPONENTS = config['rubricComponent']
+# STUDENT_RESPONSE = config['studentResponse'][0]
 
-def main():
+# def main():
 
-    # response, path = get_evaluation(Query(
-    #     rubricComponent=RUBRIC_COMPONENT,
-    #     studentResponse=STUDENT_RESPONSE
-    # ))
+#     # response, path = get_evaluation(Query(
+#     #     rubricComponent=RUBRIC_COMPONENT,
+#     #     studentResponse=STUDENT_RESPONSE
+#     # ))
 
-    # print(response.model_dump_json(indent=2))
+#     # print(response.model_dump_json(indent=2))
 
-    response_list, path_list = evaluate_question(
-        rubric_components=RUBRIC_COMPONENTS,
-        student_response=STUDENT_RESPONSE
-    )
+#     response_list, path_list = evaluate_question(
+#         rubric_components=RUBRIC_COMPONENTS,
+#         student_response=STUDENT_RESPONSE
+#     )
 
-    for response in response_list:
-        print(response.model_dump_json(indent=2))
+#     for response in response_list:
+#         print(response.model_dump_json(indent=2))
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+from app.utils.database_manager import DatabaseManager
+
+with DatabaseManager() as db:
+    question_id = db.add_question("Test")
+
+print(question_id)
